f7710cb1994a52b246a6b1bbe009f9979cf71064
Bright2196 <claireriussetcr@gmail.com>
2022-01-12 19:59:40 +0100

    yo

--- patatap/js/index.js
+++ patatap/js/index.js
@@ -1,10 +1,21 @@
+
+
 var a=1.4, b=1.1, c=2.8, d=1.5;
 var x=1, y=1;
 
 function setup() {
+
+    loadJSON("patatap/data/evolution-activite.json", gotData, 'jsonp');
     // put setup code here
-    createCanvas(500, 500);
-    stroke(150,50,20,30);
+    var canva = createCanvas(500, 500);
+    stroke(190,50,120,10);
+    canva.position(550,180);
+    
+
+}
+
+function gotData(data) {
+PrintIn(data);
 }
 
 function draw() {
@@ -20,3 +31,7 @@ function draw() {
 
 
 }
+
+//load the data
+
+//figure out the height
